579af9240a7a411d0b5ab9d369561faf9f964271
404passionFruit <hellodeadline.services@gmail.com>
2020-10-27 02:41:18 +0300

    feat: tabs keyboard support

---
+++ components/TabList.vue
@@ -0,0 +1,102 @@
+<template>
+  <section>
+    <!-- TODO: set heading level via props -->
+    <h2><slot name="heading" /></h2>
+    <div role="tablist">
+      <button
+        v-for="tab in tabs"
+        :id="`tabs-${tab.id}`"
+        :key="tab.id"
+        ref="tabButton"
+        role="tab"
+        :aria-selected="tab.id === currentTab"
+        :aria-controls="`tabs-${tab.id}`"
+        :tabindex="tab.id === currentTab ? '0' : '-1'"
+        @click="moveToContent(tab)"
+        @keyup.right.prevent="moveToNext"
+        @keyup.left.prevent="moveToPrev"
+        @keyup.home.prevent="moveToFirst"
+        @keyup.end.prevent="moveToLast"
+        v-text="tab.title"
+      />
+    </div>
+    <div
+      v-for="tab in tabs"
+      ref="tabContent"
+      :key="tab.id"
+      :hidden="tab.id === currentTab ? false : 'true'"
+      :aria-labelledby="`tabs-${tab.id}`"
+      tabindex="0"
+    >
+      <slot name="content" :tab="tab" />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    // TODO: use ids for selected tabs
+    tabs: {
+      type: Array,
+      required: true,
+    },
+    selectedTab: {
+      type: Number,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      currentTab: this.selectedTab,
+    }
+  },
+
+  computed: {
+    currentTabIndex() {
+      return this.tabs.findIndex((t) => t.id === this.currentTab)
+    },
+  },
+
+  methods: {
+    moveToContent(tab) {
+      this.currentTab = tab.id
+      this.$refs.tabContent[this.currentTabIndex].focus()
+    },
+
+    moveTo(index) {
+      this.currentTab = this.tabs[index].id
+      this.$nextTick(() => {
+        this.$refs.tabButton[index].focus()
+      })
+    },
+
+    moveToNext() {
+      if (this.currentTabIndex === this.tabs.length - 1) {
+        return
+      }
+      this.moveTo(this.currentTabIndex + 1)
+    },
+
+    moveToPrev() {
+      if (this.currentTabIndex === 0) {
+        return
+      }
+      this.moveTo(this.currentTabIndex - 1)
+    },
+
+    moveToFirst() {
+      this.moveTo(0)
+    },
+
+    moveToLast() {
+      this.moveTo(this.tabs.length - 1)
+    },
+  },
+}
+</script>
+
+<style lang="scss">
+//
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,30 @@
 <template>
   <main tabindex="-1">
     <h1 class="visually-hidden">{{ $t('pages.homepage.h1') }}</h1>
+
+    <TabList
+      :selected-tab="123"
+      :tabs="[
+        { id: 123, title: '1' },
+        { id: 223, title: '2' },
+        { id: 213, title: '3' },
+      ]"
+    >
+      <!-- TODO: translation -->
+      <template slot="heading">Выставки и события</template>
+      <template v-slot:content="slotProps">
+        <p v-if="slotProps.tab.id === 123">123 контент</p>
+        <p v-if="slotProps.tab.id === 223">аааа контент</p>
+        <p v-if="slotProps.tab.id === 213">аааапппппп контент</p>
+      </template>
+    </TabList>
   </main>
 </template>
 
 <script>
+import TabList from '@/components/TabList'
 export default {
+  components: { TabList },
   head() {
     return {
       title: this.$t('pages.homepage.h1'),
